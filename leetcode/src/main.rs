627a50cdffc1bf93206553818f7d8bb7c1a51c9a
yubaoquan <yuyuyubq@163.com>
2019-07-06 15:49:32 +0800

    update

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -2,7 +2,7 @@ use std::convert::TryInto;
 
 pub fn dominant_index(arr: Vec<i32>) -> i32 {
     let mut max = arr[0];
-    let mut max_index: i32 = 0;
+    let mut max_index: usize = 0;
     let len = arr.len();
     if len < 1 {
         return -1;
@@ -12,8 +12,6 @@ pub fn dominant_index(arr: Vec<i32>) -> i32 {
     }
 
     let mut second_max = arr[1];
-    // println!("{}, {}", max, second_max);
-
 
     if second_max > max {
         let t = max;
@@ -21,36 +19,31 @@ pub fn dominant_index(arr: Vec<i32>) -> i32 {
         second_max = t;
         max_index = 1;
     }
-    // println!("{}, {}", max, second_max);
 
-
-    for (index, item) in arr.iter().enumerate() {
-        println!("{}, {}, {}", max, second_max, item);
-        if item > &max {
+    arr.iter().enumerate().for_each(|(index, item)| {
+        let n = *item;
+        if n > max {
             second_max = max;
-            max = *item;
-            max_index = index.try_into().unwrap();
-        } else if item < &max && item > &second_max {
-            second_max = *item
+            max = n;
+            max_index = index;
+        } else if n < max && n > second_max {
+            second_max = n
         }
-    }
-
-    println!("{}, {}", max, second_max);
+    });
 
     if second_max == 0 {
-        return max_index;
+        return max_index.try_into().unwrap();
     }
 
     return if max / second_max >= 2 {
-        max_index
+        max_index.try_into().unwrap()
     } else {
         -1
     }
 }
 
 fn main() {
-    let arr: Vec<i32> = vec![0, 0, 2, 3];
+    let arr: Vec<i32> = vec![0, 0, 0, 0, 1];
     let result = dominant_index(arr);
-    // let result = 3 / 2;
     println!("result: {}", result);
 }
