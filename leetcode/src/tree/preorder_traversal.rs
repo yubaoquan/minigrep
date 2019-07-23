ae06b1a3b2da14a4d17e92ab9dc5e6fc645923d1
yubaoquan <yuyuyubq@163.com>
2019-07-23 20:02:20 +0800

    update 前序, 中序, 后序遍历

--- leetcode/src/tree/preorder_traversal.rs
+++ leetcode/src/tree/preorder_traversal.rs
@@ -3,23 +3,59 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use crate::tree::def::TreeNode;
 
+#[derive(PartialEq, Eq)]
+enum TraversalOrder {
+    Pre,
+    Middle,
+    Post,
+}
+
+/// 前序遍历
 pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     let mut result = vec![];
-    foo(root, &mut result);
+    foo(root, &mut result, &TraversalOrder::Pre);
     result
 }
 
-fn foo(root: Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>) {
+/// 中序遍历
+pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut result = vec![];
+    foo(root, &mut result, &TraversalOrder::Middle);
+    result
+}
+
+/// 后序遍历
+pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut result = vec![];
+    foo(root, &mut result, &TraversalOrder::Post);
+    result
+}
+
+fn foo(root: Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>, order: &TraversalOrder) {
     if let Some(node) = root {
         let node_borrow = node.borrow();
-        result.push(node_borrow.val);
+
+        // 先序
+        if *order == TraversalOrder::Pre {
+            result.push(node_borrow.val);
+        }
 
         if let Some(left) = &node_borrow.left {
-            foo(Some(Rc::clone(&left)), result);
+            foo(Some(Rc::clone(&left)), result, order);
+        }
+
+        // 中序
+        if *order == TraversalOrder::Middle {
+            result.push(node_borrow.val);
         }
 
         if let Some(right) = &node_borrow.right {
-            foo(Some(Rc::clone(&right)), result);
+            foo(Some(Rc::clone(&right)), result, order);
+        }
+
+        // 后序
+        if *order == TraversalOrder::Post {
+            result.push(node_borrow.val);
         }
     }
 }
@@ -32,6 +68,6 @@ pub fn test() {
     node2.left = Some(Rc::new(RefCell::new(node3)));
     node1.right = Some(Rc::new(RefCell::new(node2)));
 
-    let result = preorder_traversal(Some(Rc::new(RefCell::new(node1))));
+    let result = postorder_traversal(Some(Rc::new(RefCell::new(node1))));
     println!("{:?}", result);
 }
