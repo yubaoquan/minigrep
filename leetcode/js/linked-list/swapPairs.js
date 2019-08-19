66dfa7770717adc7a41f9ba1350c7e270b1796cb
yubaoquan <yuyuyubq@163.com>
2019-08-19 20:19:59 +0800

    update

---
+++ leetcode/js/linked-list/swapPairs.js
@@ -0,0 +1,18 @@
+function swapPairs(head) {
+  if (!head || !head.next) {
+    return head;
+  }
+  const second = head.next;
+  const next = second.next;
+  second.next = head;
+  head.next = swapPairs(next);
+  head = second;
+  return head;
+}
+
+const nodes = new Array(4).fill(0).map((t, i) => ({ val: i + 1 }));
+nodes.forEach((t, i) => {
+  t.next = nodes[i + 1] || null;
+});
+swapPairs(nodes[0])
+console.info(nodes[1]);
