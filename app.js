29ef3bfda2aba241d5aa37b407d7a648f8260a34
yubaoquan <yuyuyubq@163.com>
2019-08-30 19:38:31 +0800

    update

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const test = require('./leetcode/js/linked-list/addTwoNumbers.js');
+const test = require('./leetcode/js/linked-list/oddEvenList.js');
 
 test();
--- leetcode/js/linked-list/mergeTwoLists.js
+++ leetcode/js/linked-list/mergeTwoLists.js
@@ -1,6 +1,10 @@
+/**
+ * 合并两个有序链表
+ * https://leetcode-cn.com/explore/learn/card/linked-list/197/conclusion/762/
+ */
 function mergeTwoLists(l1, l2) {
   if (!l1 && !l2) {
-    return;
+    return l1;
   }
 
   const result = {};
@@ -14,11 +18,16 @@ function mergeTwoLists(l1, l2) {
         node.val = l2.val;
         l2 = l2.next;
       }
-      node.next
     } else {
-      if (l1) {
-
-      }
+      const listNode = l1 || l2;
+      node.val = listNode.val;
+      if (l1) { l1 = l1.next; }
+      if (l2) { l2 = l2.next; }
     }
+    if (!l1 && !l2) {
+      return result;
+    }
+    node.next = {};
+    node = node.next;
   }
 }
