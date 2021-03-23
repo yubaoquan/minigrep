98753e2cd5ad19d4a44e45eaf58cb846cf533600
yubaoquan <yuyuyubq@163.com>
2021-03-23 10:31:56 +0800

    masterMind

---
+++ leetcode/ts/easy/master_mind.ts
@@ -0,0 +1,21 @@
+/**
+ * 珠玑妙算
+ * https://leetcode-cn.com/problems/master-mind-lcci/
+ */
+
+export function masterMind(solution: string, guess: string): number[] {
+  const guessList: string[] = guess.split('');
+  const solutionList = solution.split('');
+  let allGuess = 0;
+  guessList.forEach(c => {
+    const firstIndex = solutionList.findIndex(t => t === c);
+    if (firstIndex > -1) {
+      allGuess++;
+      solutionList[firstIndex] = '_';
+    }
+  });
+  const correctGuess = guessList.filter((t, i) => solution[i] === t).length;
+  return [correctGuess, allGuess - correctGuess];
+}
+
+console.info(masterMind('RGRB', 'BBBY'));
