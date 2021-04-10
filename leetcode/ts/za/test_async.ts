0a5cf0775b903d407a1bc68a117e7cd0c84d3858
yubaoquan <yuyuyubq@163.com>
2021-04-10 21:03:59 +0800

    补充异步题

--- leetcode/ts/za/test_async.ts
+++ leetcode/ts/za/test_async.ts
@@ -31,4 +31,36 @@ async function test() {
   console.info('end');
 }
 
-test();
+// https://github.com/Advanced-Frontend/Daily-Interview-Question/issues/7
+async function test2() {
+  async function async1() {
+    console.log('async1 start');
+    await async2();
+    console.log('async1 end');
+  }
+
+  async function async2() {
+    console.log('async2');
+  }
+
+  console.log('script start');
+
+  setTimeout(() => {
+    console.log('setTimeout');
+  }, 0);
+
+  async1();
+
+  new Promise(resolve => {
+    console.log('promise1');
+    resolve(undefined);
+  }).then(() => {
+    console.log('promise2');
+  });
+
+  console.log('script end');
+}
+
+// test();
+
+test2();
