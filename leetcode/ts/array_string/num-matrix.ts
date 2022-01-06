3a8942e2330adf57bf5637ad4a875e57a75a5bde
yubaoquan <yuyuyubq@163.com>
2022-01-06 18:04:13 +0800

    前缀和, 差分数组, 滑动窗口

---
+++ leetcode/ts/array_string/num-matrix.ts
@@ -0,0 +1,57 @@
+/**
+ * https://leetcode-cn.com/problems/range-sum-query-2d-immutable/
+ */
+
+export class NumMatrix {
+  preSum: number[][] = [[0]];
+
+  constructor(matrix: number[][]) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    if (!m || !n) return;
+
+    const { preSum } = this;
+
+    for (let i = 1; i <= m; i += 1) {
+      preSum[i] = [0];
+
+      for (let j = 1; j <= n; j += 1) {
+        preSum[i - 1][j] ||= 0;
+        preSum[i][j - 1] ||= 0;
+        preSum[i - 1][j - 1] ||= 0;
+
+        preSum[i][j] = preSum[i - 1][j]
+          + preSum[i][j - 1]
+          + matrix[i - 1][j - 1]
+          - preSum[i - 1][j - 1];
+      }
+    }
+  }
+
+  sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+    const { preSum } = this;
+
+    return preSum[row2 + 1][col2 + 1]
+      - preSum[row1][col2 + 1]
+      - preSum[row2 + 1][col1]
+      + preSum[row1][col1];
+  }
+}
+
+const test = () => {
+  const numMatrix = new NumMatrix([
+    [3, 0, 1, 4, 2],
+    [5, 6, 3, 2, 1],
+    [1, 2, 0, 1, 5],
+    [4, 1, 0, 1, 7],
+    [1, 0, 3, 0, 5],
+  ]);
+  const a = numMatrix.sumRegion(2, 1, 4, 3); // return 8 (红色矩形框的元素总和)
+  const b = numMatrix.sumRegion(1, 1, 2, 2); // return 11 (绿色矩形框的元素总和)
+  const c = numMatrix.sumRegion(1, 2, 2, 4); // return 12 (蓝色矩形框的元素总和)
+  const success = a === 8 && b === 11 && c === 12;
+  console.info(a, b, c);
+  console.info(success);
+};
+
+test();
---
+++ leetcode/ts/array_string/sliding-window.ts
@@ -0,0 +1,38 @@
+/* 滑动窗⼝算法框架 */
+export const slidingWindow = (s: string, t: string) => {
+  const need: Record<string, number> = {};
+  const _window;
+
+  for (let i = 0; i < t.length; i += 1) {
+    need[t[i]] = need[t[i]] ? need[t[i]] + 1 : 1;
+  }
+
+  let left = 0;
+  let right = 0;
+  const valid = 0;
+  while (right < s.length) {
+  // c 是将移⼊窗⼝的字符
+    const c = s[right];
+
+    // 右移窗⼝
+    right++;
+
+    // 进⾏窗⼝内数据的⼀系列更新
+    // ...
+    /** * debug 输出的位置 ** */
+    console.info(left, right);
+
+    /** ***************** */
+    // 判断左侧窗⼝是否要收缩
+    while (/* window needs shrink */ true) {
+      // d 是将移出窗⼝的字符
+      const d = s[left];
+
+      // 左移窗⼝
+      left++;
+
+      // 进⾏窗⼝内数据的⼀系列更新
+      // ...
+    }
+  }
+};
